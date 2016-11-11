bb9fc83be22e3dae0c7af62db7fe422d7d605863
Leonardo <leonardotreze@gmail.com>
2016-11-11 02:44:03 -0200

    impl guid parcial

--- app/src/main/java/com/leonardoserra/tarefasdecasa/MainActivity.java
+++ app/src/main/java/com/leonardoserra/tarefasdecasa/MainActivity.java
@@ -1,19 +1,28 @@
 package com.leonardoserra.tarefasdecasa;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import com.startapp.android.publish.StartAppAd;
 import com.startapp.android.publish.StartAppSDK;
 import com.startapp.android.publish.splash.SplashConfig;
 
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
 
     private WebView myWebView;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor e;
+    private String guid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +36,43 @@ public class MainActivity extends AppCompatActivity {
         );
         setContentView(R.layout.activity_main);
 
+
         myWebView = (WebView) findViewById(R.id.webview);
         myWebView.setWebViewClient(new WebViewClient());
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         myWebView.loadUrl("http://10.0.2.2:8082/");
+
+
+        sharedPreferences = this.getSharedPreferences("", Context.MODE_PRIVATE);
+        e = sharedPreferences.edit();
+        guid = sharedPreferences.getString("guid", null);
+        if (guid == null){
+            guid = UUID.randomUUID().toString();
+            e.putString("guid", guid);
+            e.commit();
+        }
+        //myWebView.loadUrl("javascript:sample('" + guid + "')");
+        myWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
+    }
+
+    public class WebAppInterface {
+        Context mContext;
+
+        /** Instantiate the interface and set the context */
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        /** Show a toast from the web page */
+        @JavascriptInterface
+        public void showToast(String toast) {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+        }
+        @JavascriptInterface
+        public String getGuid() {
+            return guid;
+        }
     }
 
     @Override
